3bf127bc7780d5ef72ed9248fcfa11f3d8954bf6
pedrofavero <112567328+pedrofavero@users.noreply.github.com>
2025-10-22 22:10:10 -0300

    feat(core)Ç adição da interface para usuário no main

--- Main.java
+++ Main.java
@@ -1,21 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         ArvoreBinariaMorse arvore = new ArvoreBinariaMorse();
         arvore.inicializar();
+        Scanner leitor = new Scanner(System.in);
 
+        int opcao = 1;
 
-        // Testes como no enunciado 
-        System.out.println(arvore.buscar("..."));         // Saída esperada: S
-        System.out.println(arvore.buscar("---"));         // Saída esperada: O
-        System.out.println(arvore.buscar("... --- ..."));   // Saída esperada: SOS
-        
-        // Teste de remoção (agora imprimindo o retorno)
-        System.out.println("\n--- Teste de Remocao ---");
-        System.out.println("Buscando '---' (O): " + arvore.buscar("---"));
-        arvore.remover("---");
-        System.out.println("Removido '---' (O)");
-        System.out.println("Buscando '---' (vazio): " + arvore.buscar("---"));
+        while(opcao != 0){
+            System.out.println("[0] Sair\n[1] Traduzir caracter para morse\n[2] Traduzir morse para caracter\n[3] Inserir caracter\n[4] Remover morse");
 
-        System.out.println(arvore.traduzir(arvore.raiz, "A"));
+            System.out.print("Digite sua escola: ");
+            opcao = leitor.nextInt();
+            leitor.nextLine();
+
+            if(opcao == 1){
+                System.out.print("Digite o caracter: ");
+                String caracter = leitor.nextLine();
+
+                System.out.println(arvore.traduzir(arvore.raiz, caracter));
+            }
+            else if(opcao == 2){
+                System.out.print("Digite o codigo morse: ");
+                String entrada = leitor.nextLine();
+
+                System.out.println(arvore.buscar(entrada));
+            }
+
+            else if(opcao == 3){
+                System.out.print("Digite o codigo morse: ");
+                String codigo = leitor.nextLine();
+
+                System.out.print("\nDigite o caracter: ");
+                String caracter = leitor.nextLine();
+
+                arvore.inserir(codigo, caracter);
+            }
+
+            else if(opcao == 4){
+                System.out.print("Digite o codigo: ");
+                String codigo = leitor.nextLine();
+
+                arvore.remover(codigo);
+            }
+        }
+
     }
 }
